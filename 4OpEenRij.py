48ac080c2ccbe11348f894f29b0e897e4d8085e7
WijnandVanDijk <60653370+WijnandVanDijk@users.noreply.github.com>
2020-06-22 00:39:57 +0200

    Begin gemaakt met de AI
    Eerst de non AI code gekopieerd en die een beetje aangepast

--- 4OpEenRij.py
+++ 4OpEenRij.py
@@ -2,6 +2,7 @@ import numpy as np
 import pygame
 import sys
 import math
+import random
 
 from pygame import mixer
 from pygame.locals import *
@@ -16,6 +17,164 @@ YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 
 def connectfour_with_ai():
+
+    aantal_rijen = 6
+    aantal_kolommen = 7
+
+    MENS = 0
+    AI = 1
+
+    LEEG = 0
+    MENS_STEEN = 1
+    AI_STEEN = 2
+
+    def maak_bord():
+        bord = np.zeros((aantal_rijen, aantal_kolommen))  # np.zeros maakt een matrix aan gevuld met nullen
+        return bord
+
+    def leg_steen(bord, rij, kolom, steen):
+        bord[rij][kolom] = steen
+
+    def geldige_locatie(bord, kolom):
+        return bord[aantal_rijen - 1][kolom] == 0
+
+    def volgende_open_rij(bord, kolom):
+        for r in range(aantal_rijen):
+            if bord[r][kolom] == 0:
+                return r
+
+    def print_bord(bord):
+        print(np.flip(bord, 0))
+
+    def winnende_zet(bord, steen):
+        for k in range(aantal_kolommen - 3):
+            for r in range(aantal_rijen):
+                if bord[r][k] == steen and bord[r][k + 1] == steen and bord[r][k + 2] == steen and bord[r][
+                    k + 3] == steen:
+                    return True
+
+        for k in range(aantal_kolommen):
+            for r in range(aantal_rijen - 3):
+                if bord[r][k] == steen and bord[r + 1][k] == steen and bord[r + 2][k] == steen and bord[r + 3][
+                    k] == steen:
+                    return True
+
+        for k in range(aantal_kolommen - 3):
+            for r in range(aantal_rijen - 3):
+                if bord[r][k] == steen and bord[r + 1][k + 1] == steen and bord[r + 2][k + 2] == steen and bord[r + 3][
+                    k + 3] == steen:
+                    return True
+
+        for k in range(aantal_kolommen - 3):
+            for r in range(3, aantal_rijen):
+                if bord[r][k] == steen and bord[r - 1][k + 1] == steen and bord[r - 2][k + 2] == steen and bord[r - 3][
+                    k + 3] == steen:
+                    return True
+
+    def punten_positie(bord, steen):
+        pass
+
+    def teken_bord(bord):
+        for k in range(aantal_kolommen):
+            for r in range(aantal_rijen):
+                pygame.draw.rect(Gamescherm, BLUE,
+                                 (k * squaresize, r * squaresize + squaresize, squaresize, squaresize))
+                pygame.draw.circle(Gamescherm, BLACK, (
+                    int(k * squaresize + squaresize / 2), int(r * squaresize + squaresize + squaresize / 2)), radius)
+
+        for k in range(aantal_kolommen):
+            for r in range(aantal_rijen):
+                if bord[r][k] == 1:
+                    pygame.draw.circle(Gamescherm, RED, (
+                        int(k * squaresize + squaresize / 2), hoogte - int(r * squaresize + squaresize / 2)), radius)
+                elif bord[r][k] == 2:
+                    pygame.draw.circle(Gamescherm, YELLOW, (
+                        int(k * squaresize + squaresize / 2), hoogte - int(r * squaresize + squaresize / 2)), radius)
+        pygame.display.update()
+
+    bord = maak_bord()
+    print_bord(bord)
+    game_over = False
+    beurd = 0
+
+    pygame.init()  # bron voor pygame: https://www.pygame.org/docs/ref/draw.html
+
+    squaresize = 100  # in pixels
+
+    breedte = aantal_kolommen * squaresize
+    hoogte = (aantal_rijen + 1) * squaresize  # +1 zodat je ziet waar je de zet doet
+
+    size = (breedte, hoogte)
+
+    radius = int(squaresize / 2 - 5)
+
+    Gamescherm = pygame.display.set_mode(size)
+    teken_bord(bord)
+    pygame.display.update()
+
+    myfont = pygame.font.SysFont('monospace', 75)
+
+    beurd = random.randint(MENS, AI)
+
+    while not game_over:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(Gamescherm, BLACK, (0, 0, breedte, squaresize))
+                posx = event.pos[0]
+                if beurd == MENS:
+                    pygame.draw.circle(Gamescherm, RED, (posx, int(squaresize / 2)), radius)
+            pygame.display.update()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(Gamescherm, BLACK, (0, 0, breedte, squaresize))
+                if beurd == MENS:
+                    posx = event.pos[0]
+                    kolom = int(math.floor(posx / squaresize))
+
+                    if geldige_locatie(bord, kolom):
+                        rij = volgende_open_rij(bord, kolom)
+                        leg_steen(bord, rij, kolom, 1)
+
+                        if winnende_zet(bord, 1):
+                            label = myfont.render("Speler 1 wint!", 1, RED)
+                            Gamescherm.blit(label, (40, 10))
+                            game_over = True
+
+                        beurd += 1
+                        beurd = beurd % 2
+
+                        print_bord(bord)
+                        teken_bord(bord)
+
+                if beurd == AI and not game_over:
+
+                    kolom = random.randint(0, aantal_kolommen-1)
+
+                    if geldige_locatie(bord, kolom):
+                        pygame.time.wait(500)
+                        rij = volgende_open_rij(bord, kolom)
+                        leg_steen(bord, rij, kolom, 2)
+
+                        if winnende_zet(bord, 2):
+                            label = myfont.render("Speler 2 wint!", 1, YELLOW)
+                            Gamescherm.blit(label, (40, 10))
+                            game_over = True
+
+                        print_bord(bord)
+                        teken_bord(bord)
+
+                        beurd += 1
+                        beurd = beurd % 2
+
+                if game_over:
+                    pygame.time.wait(5000)  # in miliseconds, dus 5 seconden
+                    pygame.display.quit()
+                    pygame.quit()
+
 
 def connectfour_no_ai():
 
@@ -385,4 +544,5 @@ def gui():
     main_menu()
 
 
-gui()
+#connectfour_with_ai()
+#gui()
